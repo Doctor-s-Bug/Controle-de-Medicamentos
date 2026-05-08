1f179e359822750a28d50d6d41583f84274a1fc1
thig0S_ <thiagokovalski5@gmail.com>
2026-05-08 15:42:17 -0300

    Implementada a classe Paciente e começo da classe TelaPaciente

--- ControleDeMedicamentos.ConsoleApp/ModuloPacientes/Paciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPacientes/Paciente.cs
@@ -1,21 +1,72 @@
 using System;
+using System.Runtime.Serialization;
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
 
 public class Paciente : EntidadeBase
 {
-  public string Nome { get; set; }
+  public Paciente(string nome, string telefone, string cartaoSus, string cpf)
+  {
+    Nome = nome;
+    Telefone = telefone;
+    CartaoSus = cartaoSus;
+    Cpf = cpf;
+  }
 
+  public string Nome { get; set; }
   public string Telefone { get; set; }
+  public string CartaoSus { get; set; }
+  public string Cpf { get; set; }
+
 
   public override void AtualizarDados(EntidadeBase entidadeAtualizada)
   {
-    throw new NotImplementedException();
+    //cast = confia
+    Paciente? pacienteAtualizado = (Paciente?)entidadeAtualizada;
+
+    Nome = pacienteAtualizado.Nome;
+    Telefone = pacienteAtualizado.Telefone;
+    CartaoSus = pacienteAtualizado.CartaoSus;
+    Cpf = pacienteAtualizado.Cpf;
   }
 
   public override List<string> Validar()
   {
-    throw new NotImplementedException();
+    List<string> erros = new List<string>();
+
+    if (String.IsNullOrWhiteSpace(Nome))
+      erros.Add("Nome não pode ser Null!;");
+
+    else if (Nome.Length < 3 || Nome.Length > 100)
+      erros.Add("Nome deve conter entre 3 a 100 caracteres!;");
+
+    string telefoneEncurtado = Telefone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "");
+    bool contemLetraOuSimbolo = false;
+    int contadorDigitos = 0;
+
+    for (int i = 0; i < telefoneEncurtado.Length; i++)
+    {
+      char c = telefoneEncurtado[i];
+      if (char.IsDigit(c))
+        contadorDigitos++;
+      else
+      {
+        contemLetraOuSimbolo = true;
+        break;
+      }
+    }
+
+    if (contadorDigitos < 10 || contadorDigitos > 11)
+      erros.Add("O campo \"Telefone\" deve conter entre 10 e 11 dígitos;");
+
+    if (contemLetraOuSimbolo)
+      erros.Add("O campo \"Telefone\" deve conter apenas dígitos;");
+
+    if (Cpf.Length != 14)
+      erros.Add("O campo CPF deve conter 14 digitos!;");
+
+    return erros;
   }
 }
--- ControleDeMedicamentos.ConsoleApp/ModuloPacientes/TelaPaciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPacientes/TelaPaciente.cs
@@ -1,21 +1,33 @@
 using System;
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
-
 namespace ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
 
 public class TelaPaciente : TelaBase<Paciente>, ITelaCrud, ITelaOpcoes
 {
-  public TelaPaciente(IRepositorio<Paciente> repositorio) : base("Paciente", repositorio)
-  {
-  }
+    public TelaPaciente(string nomeEntidade, IRepositorio<Paciente> repositorio) : base("Paciente", repositorio)
+    {
 
-  public override void VisualizarTodos(bool deveExibirCabecalho)
-  {
-    Console.WriteLine("Visualizando todos os pacientes...");
-  }
+    }
 
-  protected override Paciente ObterDadosCadastrais()
-  {
-    return new Paciente();
-  }
+    public override void VisualizarTodos(bool deveExibirCabecalho)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Paciente ObterDadosCadastrais()
+    {
+        System.Console.Write("Digite o NOME do paciente: ");
+        string? nome = Console.ReadLine();
+
+        System.Console.Write("Digite o TELEFONE do paciente: ");
+        string? telefone = Console.ReadLine();
+
+        System.Console.Write("Digite o Cartao SUS do paciente: ");
+        string? cartaoSus = Console.ReadLine();
+
+        System.Console.Write("Digite o CPF do paciente: ");
+        string? cpf = Console.ReadLine();
+
+        return new Paciente(nome, telefone, cartaoSus, cpf);
+    }
 }
