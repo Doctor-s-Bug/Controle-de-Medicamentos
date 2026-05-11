8b97514562944065513b64e8b779eae18b8b4e2c
Davi Andrade <contatodaviandrade20@gmail.com>
2026-05-11 17:13:47 -0300

    Criado classe Funcionario

--- ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/Funcionario.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/Funcionario.cs
@@ -8,11 +8,49 @@ public class Funcionario : EntidadeBase
 
     public override void AtualizarDados(EntidadeBase entidadeAtualizada)
     {
-        throw new NotImplementedException();
+        Funcionario funcionarioAtualizado = (Funcionario)entidadeAtualizada;
+
+        Nome = funcionarioAtualizado.Nome;
+        Telefone = funcionarioAtualizado.Telefone;
+        Cpf = funcionarioAtualizado.Cpf;
     }
 
     public override List<string> Validar()
     {
-        throw new NotImplementedException();
+        List<string> erros = new List<string>();
+
+        if (String.IsNullOrWhiteSpace(Nome))
+            erros.Add("Nome não pode ser Null;");
+
+        else if (Nome.Length < 3 || Nome.Length > 100)
+            erros.Add("Nome deve conter entre 3 a 100 caracteres!;");
+
+
+        string telefoneEncurtado = Telefone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "");
+        bool contemLetraOuSimbolo = false;
+        int contadorDigitos = 0;
+
+        for (int i = 0; i < telefoneEncurtado.Length; i++)
+        {
+            char c = telefoneEncurtado[i];
+            if (char.IsDigit(c))
+                contadorDigitos++;
+            else
+            {
+                contemLetraOuSimbolo = true;
+                break;
+            }
+        }
+
+        if (contadorDigitos < 10 || contadorDigitos > 11)
+            erros.Add("O campo \"Telefone\" deve conter entre 10 e 11 dígitos;");
+
+        if (contemLetraOuSimbolo)
+            erros.Add("O campo \"Telefone\" deve conter apenas dígitos;");
+
+        if (Cpf.Length != 11)
+            erros.Add("O campo Cpf deve conter 11 digitos!;");
+
+        return erros;
     }
 }
